c05165366659832ee6c4ab76de5d320eb98aa5ef
Ever Cifuentes <everbilly35@gmail.com>
2019-04-14 22:26:30 -0600

    [ADDED] add lines in file main.js for description of the console.log

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Comentario 1 linea
+
+/*Comentario 
+  Multiple Linea
+*/
+//Mostrar en la Consola
+console.log('Bienvenido a Javascript');
+var prueba = 'Hola Curso';
+console.log(prueba);
+
+console.log([1,2,3,4,5,6]);
+
+console.log({nombre:'Ever', apellido:'Cifuentes'});
+
+console.table({nombre:'Ever', apellido:'Cifuentes'});
